ffc02046bfdece4634d04ef72901bb9b2e0dd199
Paul Chote <paul@chote.net>
2011-02-03 16:44:37 +1300

    Center the aperture of the star

--- reduction/reduction.py
+++ reduction/reduction.py
@@ -19,81 +19,9 @@ import math
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-# rect: x y width height for the target region
-# data: total image data
-def display_region(rect, data):
-    # Copy the region specified by `rectangle` into `out`
-    out = numpy.empty([rect[3],rect[2]])
-    for j in range(0,rect[3],1):
-      for i in range(0,rect[2],1):
-        out[j,i] = data[j+rect[1],i+rect[0]]
-
-    # low-pass filter the image with a 2d binomial filter
-    #filt = scipy.signal.convolve2d(out, [[0.0625,0.125,0.0625],[0.125,0.25,0.125],[0.0625,0.125,0.0625]])
-
-    # Calculate the background level as the mode of the histogram
-    # which is estimated by 3*mean - 2*median
-    filt = out
-    bg = 3*numpy.mean(filt) - 2*numpy.median(filt)
-
-    # d = ds9.ds9('rangahau')
-    #    d.set_np2arr(filt)
-    #    d.set('crosshair %f %f' % (xc,yc))
-    
-    # Draw a histogram
-    # flattened = filt.reshape(-1)  
-    # n, bins, patches = plt.hist(flattened, 50, normed=1, facecolor='green', alpha=0.75)  
-    # plt.xlabel('Counts')
-    # plt.ylabel('Probability')
-    # plt.title(r'$\mathrm{Histogram\ of\ region:}$')
-    # plt.grid(True)
-    # plt.show()
-    
-    # Calculate the standard deviation of the data from the mode (use numpy.std to give it from the mean)
-    std = math.sqrt(numpy.mean(abs(out - bg)**2))
-    print std
-
-    # Threshold the image to eliminate background
-    # Todo use 3sigma above the background
-    thresh = numpy.empty([rect[3],rect[2]])
-    t = bg
-    for j in range(0,rect[3],1):
-        for i in range(0,rect[2],1):
-            if filt[j,i] < bg + 3*std:
-                thresh[j,i] = 0
-            else:
-                thresh[j,i] = filt[j,i] - bg
-
-    # Calculate the center points
-    num = 0
-    denom = 0
-    for i in range(0,rect[2],1):
-        m = 0
-        for j in range(0,rect[3],1):
-            m += thresh[j,i]
-        num += i*m
-        denom += m
-  
-    print "xc = %f/%f = %f " % (num, denom, num/denom)
-    xc = num/denom
-
-    num = 0
-    denom = 0
-    for j in range(0,rect[3],1):
-        m = 0
-        for i in range(0,rect[2],1):
-            m += thresh[j,i]
-        num += j*m
-        denom += m
-    yc = num/denom
-
-    print "yc = %f/%f = %f " % (num, denom, num/denom)
-
-    # Display the region in ds9
-    d = ds9.ds9('rangahau')
-    d.set_np2arr(thresh)
-    d.set('crosshair %f %f' % (xc,yc))
-
+# Prompts the user for a region to search
+#   Takes a ds9 object to preview the region in
+#   Returns the x,y,r1,r2 coordinates of the region
 def prompt_region(d):
     x = y = r1 = r2 = -1
     done = False
@@ -123,6 +51,45 @@ def prompt_region(d):
     
     return x, y, r1, r2
 
+# Find the center of the star within the inner circle
+#   Takes the search annulus and imagedata
+#   Returns x,y coordinates for the star center
+def center_aperture(x, y, r1, bg, std, imagedata):
+    # Copy the data within the inside (circular) apeture
+    # and threshold / subtract the background level
+    data = numpy.empty([2*r1,2*r1])
+    for j in range(0,2*r1,1):
+        for i in range(0,2*r1,1):
+            xx = x - r1 + i
+            yy = y - r1 + j
+            
+            d2 = (x-xx)**2 + (y-yy)**2
+            if d2 < r1*r1 and imagedata[yy,xx] > bg + 3*std:
+                data[j,i] = imagedata[yy,xx] - bg
+            else:
+                data[j,i] = 0
+
+    # Calculate the x and y marginals
+    # (collapse the image into a 1d line in each axis)
+    xm = data.sum(axis=0)
+    ym = data.sum(axis=1)
+
+    # Total counts to normalize by
+    tot = xm.sum()
+
+    # Calculate x and y moments
+    xc,yc = 0,0
+    for i in range(0,2*r1,1):
+        xc += i*xm[i]/tot
+        yc += i*ym[i]/tot
+    
+    # Convert back to image coords
+    xc += x - r1
+    yc += y - r1
+    return xc, yc
+
+# Takes the search annulus and imagedata
+# Returns the background intensity plus standard deviation
 def calculate_background(x, y, r1, r2, imagedata):
     mask = numpy.ones(imagedata.shape)
     print x,y,r1,r2
@@ -133,6 +100,7 @@ def calculate_background(x, y, r1, r2, imagedata):
                 mask[j,i] = 0
     
     masked = numpy.ma.masked_array(imagedata, mask=mask)
+    # Calculate the mode of the background
     bg = 3*numpy.mean(masked) - 2*numpy.ma.extras.median(masked)
     std = math.sqrt(numpy.mean(abs(masked - bg)**2))
     return bg, std
@@ -145,6 +113,7 @@ def main():
         hdulist.info()
 
         imagedata = hdulist[0].data
+        
         # Copy imagedata into a format ds9 can display (this is an ugly hack!)
         out = numpy.empty(imagedata.shape)
         for j in range(0,imagedata.shape[0],1):
@@ -161,16 +130,10 @@ def main():
         print "Background level: %f" % bg
         print "Standard Deviation: %f" % std
         
-        thresh = numpy.empty(imagedata.shape)
-        for j in range(0,imagedata.shape[0],1):
-            for i in range(0,imagedata.shape[1],1):
-                if imagedata[j,i] < bg + 3*std:
-                    thresh[j,i] = 0
-                else:
-                    thresh[j,i] = imagedata[j,i] - bg
-                    
-        d.set_np2arr(thresh)
-                  
+        # Compute improved x,y
+        x,y = center_aperture(x, y, r1, bg, std, imagedata)
+        d.set('crosshair %f %f' % (x+1,y+1))
+        print x,y
         #display_region([x - r2,460,150,150], imagedata)
         hdulist.close()
     else:
