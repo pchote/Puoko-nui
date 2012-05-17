5d61d88d191ee66376ef40267ce6013ed49124ee
Paul Chote <paul@chote.net>
2012-05-17 15:18:02 +1200

    Update frame_available.sh to use tsreduce

--- frame_available.sh
+++ frame_available.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR=`dirname $1`
-#python ./reduction/reduction.py ${DIR:1}/
-#python ./reduction/plot.py ${DIR:1}/
+FILE=${DIR}/undefined.dat
+tsreduce update ${FILE} 1> /dev/null 2> /dev/null
+tsreduce plot ${FILE} 1> /dev/null 2> /dev/null
