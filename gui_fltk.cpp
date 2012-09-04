2cdf622349cc77008f9cc8cf08839f1761ad1d26
Paul Chote <paul@chote.net>
2012-09-05 09:33:05 +1200

    Mock up left side info panels with dummy info

--- gui_fltk.cpp
+++ gui_fltk.cpp
@@ -5,11 +5,9 @@
  * published by the Free Software Foundation. For more information, see LICENSE.
  */
 
-#include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
-
 #include "gui_fltk.h"
+
+FLTKGui *gui;
 
 void init_log_gui()
 {
@@ -23,14 +21,7 @@ void add_log_line(char *msg)
 
 void pn_ui_new()
 {
-    Fl_Window *window = new Fl_Window(340,180);
-    Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
-    box->box(FL_UP_BOX);
-    box->labelfont(FL_BOLD+FL_ITALIC);
-    box->labelsize(36);
-    box->labeltype(FL_SHADOW_LABEL);
-    window->end();
-    window->show();
+    gui = new FLTKGui();
 }
 
 bool pn_ui_update()
@@ -40,5 +31,82 @@ bool pn_ui_update()
 
 void pn_ui_free()
 {
-    
+    delete gui;
+}
+
+Fl_Group *FLTKGui::CreateGroupBox(int y, int h, const char *label)
+{
+    int b[4] = {10, y, 230, h};
+    Fl_Group *output = new Fl_Group(b[0], b[1], b[2], b[3], label);
+    output->box(FL_ENGRAVED_BOX);
+    output->align(FL_ALIGN_INSIDE|FL_ALIGN_TOP);
+    output->labelsize(14);
+    output->labelfont(FL_BOLD);
+    return output;
+}
+
+Fl_Output *FLTKGui::CreateOutputLabel(int y, const char *label)
+{
+    int b[5] = {105, y, 130, 14};
+    Fl_Output *output = new Fl_Output(b[0], b[1], b[2], b[3], label);
+    output->box(FL_NO_BOX);
+    output->labelsize(14);
+    output->textsize(13);
+    output->textfont(FL_BOLD);
+    output->value("2011-07-30 18:06:49sdfsdfsddssdsdfdsfdssd");
+    return output;
+}
+
+
+void FLTKGui::CreateTimerGroup()
+{
+    int y = 10, margin = 20;
+    m_timerGroup = CreateGroupBox(y, 105, "Timer Information"); y += 25;
+    m_timerStatusOutput = CreateOutputLabel(y, "Status:"); y += margin;
+    m_timerPCTimeOutput = CreateOutputLabel(y, "PC Time:"); y += margin;
+    m_timerUTCTimeOutput = CreateOutputLabel(y, "UTC Time:"); y += margin;
+    m_timerCountdownOutput = CreateOutputLabel(y, "Countdown:");
+    m_timerGroup->end();
+}
+
+void FLTKGui::CreateCameraGroup()
+{
+    // x,y,w,h
+    int y = 125, margin = 20;
+    m_cameraGroup = CreateGroupBox(y, 65, "Camera Information"); y += 25;
+    m_cameraStatusOutput = CreateOutputLabel(y, "Status:"); y += margin;
+    m_cameraTemperatureOutput = CreateOutputLabel(y, "Temperature:");
+    m_cameraGroup->end();
+}
+
+void FLTKGui::CreateAcquisitionGroup()
+{
+    int y = 200, margin = 20;
+    m_acquisitionGroup = CreateGroupBox(y, 145, "Acquisition"); y += 25;
+    m_acquisitionObserversOutput = CreateOutputLabel(y, "Observers:"); y += margin;
+    m_acquisitionTypeOutput = CreateOutputLabel(y, "Type:"); y += margin;
+    m_acquisitionTargetOutput = CreateOutputLabel(y, "Target:"); y += margin;
+    m_acquisitionRemainingOutput = CreateOutputLabel(y, "Remaining:"); y += margin;
+    m_acquisitionExposureOutput = CreateOutputLabel(y, "Exposure:"); y += margin;
+    m_acquisitionFilenameOutput = CreateOutputLabel(y, "Filename:");
+    m_acquisitionGroup->end();
+}
+
+FLTKGui::FLTKGui()
+{
+	// Create the main window
+	m_mainWindow = new Fl_Window(680, 400, "Acquisition Control");
+    m_mainWindow->user_data((void*)(this));
+
+    CreateTimerGroup();
+    CreateCameraGroup();
+    CreateAcquisitionGroup();
+
+	m_mainWindow->end();
+	m_mainWindow->show();
+}
+
+FLTKGui::~FLTKGui()
+{
+
 }
--- gui_fltk.h
+++ gui_fltk.h
@@ -5,6 +5,12 @@
  * published by the Free Software Foundation. For more information, see LICENSE.
  */
 
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Text_Display.H>
+
 #ifndef GUI_FLTK_H
 #define GUI_FLTK_H
 
@@ -16,5 +22,44 @@ extern "C"
     bool pn_ui_update();
     void pn_ui_free();
 }
+
+
+
+class FLTKGui
+{
+public:
+	FLTKGui();
+	~FLTKGui();
+
+private:
+    static Fl_Group *CreateGroupBox(int y, int h, const char *label);
+    static Fl_Output *CreateOutputLabel(int y, const char *label);
+    void CreateTimerGroup();
+    void CreateCameraGroup();
+    void CreateAcquisitionGroup();
+
+    Fl_Window*			m_mainWindow;
+
+    // Timer info
+    Fl_Group *m_timerGroup;
+    Fl_Output *m_timerStatusOutput;
+    Fl_Output *m_timerPCTimeOutput;
+    Fl_Output *m_timerUTCTimeOutput;
+    Fl_Output *m_timerCountdownOutput;
+
+    // Camera info
+    Fl_Group *m_cameraGroup;
+    Fl_Output *m_cameraStatusOutput;
+    Fl_Output *m_cameraTemperatureOutput;
+
+    // Acquisition info
+    Fl_Group *m_acquisitionGroup;
+    Fl_Output *m_acquisitionObserversOutput;
+    Fl_Output *m_acquisitionTypeOutput;
+    Fl_Output *m_acquisitionTargetOutput;
+    Fl_Output *m_acquisitionRemainingOutput;
+    Fl_Output *m_acquisitionExposureOutput;
+    Fl_Output *m_acquisitionFilenameOutput;
+};
 
 #endif
