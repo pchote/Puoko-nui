7e190b293e44ad191d87789b8d0f502e4500df72
Paul Chote <paul@chote.net>
2012-09-07 13:35:04 +1200

    Update year and indentation in file headers

--- gui_fltk.cpp
+++ gui_fltk.cpp
@@ -1,12 +1,11 @@
 /*
- * Copyright 2012 Paul Chote
+ * Copyright 2010-2012 Paul Chote
  * This file is part of Puoko-nui, which is free software. It is made available
  * to you under the terms of version 3 of the GNU General Public License, as
  * published by the Free Software Foundation. For more information, see LICENSE.
  */
 
 #include <math.h>
-
 #include "gui_fltk.h"
 
 FLTKGui *gui;
