e8c3ea787c1d26fb99a79f8701a62c369cc93639
Paul Chote <paul@chote.net>
2012-09-07 18:18:59 +1200

    Fix preview scripting on windows

--- preview.ps1
+++ preview.ps1
@@ -6,6 +6,8 @@ $previewdir = $PWD -replace '\\','/'
 
 # Only update the preview if the appropriate ds9 window exists
 $running = xpaaccess -n Online_Preview
-if ($running -ne "0") {
+if ($running -eq "0") {
+    & ds9 -title Online_Preview
+} else {
     & xpaset -p Online_Preview file "$previewdir/preview.fits.gz"
 }
--- startup.ps1
+++ startup.ps1
@@ -1,2 +1,13 @@
-# Do Nothing. Launching ds9 from powershell doesn't
-# start xpa, so require the user to do this manually.
+# Add the directories containing the xpaset and ds9 binaries to the path
+$env:path += ';c:/ds9'
+
+# Start the XPA server in the background.
+# It will automatically terminate if another
+# instance is running, so this is safe.
+Start-Job -scriptblock { & xpans }
+
+# Launch ds9 if necessary
+$running = xpaaccess -n Online_Preview
+if ($running -eq "0") {
+    & ds9 -title Online_Preview
+}
