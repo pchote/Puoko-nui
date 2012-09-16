09a9347437606750268fe66828da87c9d46ed6f4
Paul Chote <paul@chote.net>
2012-09-17 09:58:20 +1200

    Remove nonfunctional xpa startup from startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,11 +1,6 @@
 # Add the directories containing the xpaset and ds9 binaries to the path
 $env:path += ';c:/ds9'
 
-# Start the XPA server in the background.
-# It will automatically terminate if another
-# instance is running, so this is safe.
-Start-Job -scriptblock { & xpans }
-
 # Launch ds9 if necessary
 $running = xpaaccess -n Online_Preview
 if ($running -eq "0") {
