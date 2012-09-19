c511a91d4d36b88bf397ea3972c0de24782470a0
Paul Chote <paul@chote.net>
2012-09-20 08:49:36 +1200

    Make frame_available.sh more robust

--- frame_available.sh
+++ frame_available.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+
+# Path to online reduction file to update
+# By default, look for the given name in the frame directory
 DIR=`dirname $1`
-FILE=${DIR}/ec04sim.dat
-tsreduce update ${FILE} &> last_update.log
+FILE=${DIR}/undefined.dat
 
-if [ "$?" -ne "0" ]
-then
-	echo "tsreduce update FAILED. See last_update.log"
-fi
+# Do the reduction if the file is found
+if [ -f ${FILE} ]; then
+	tsreduce update ${FILE} &> last_update.log
+	if [ "$?" != '0' ]; then
+		echo 'tsreduce update FAILED. See last_update.log'
+		exit 1
+	fi
 
-tsreduce plot ${FILE} &> last_plot.log
-if [ "$?" -ne "0" ]
-then
-	echo "tsreduce plot FAILED. See last_plot.log"
+	# Windows does not offer a persistent plot device,
+	# so plot images which are then displayed in a web page
+	PLOTDEVICE=''
+	if [[ "${OSTYPE}" == 'msys' ]]; then
+		PLOTDEVICE='online_ts.gif/gif online_dft.gif/gif'
+	fi
+
+	tsreduce plot ${FILE} PLOTDEVICE &> last_plot.log
+	if [ "$?" != '0' ]; then
+		echo 'tsreduce plot FAILED. See last_plot.log'
+	fi
 fi
