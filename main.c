d03c93f14bae3b149931e0f26890765c06973884
Paul Chote <paul@chote.net>
2012-10-04 17:46:28 +1300

    Use one function for saving frames and previews.

--- main.c
+++ main.c
@@ -13,15 +13,15 @@
 #include <sys/time.h>
 #include <pthread.h>
 #include <string.h>
+#include <fitsio.h>
 #include "common.h"
 #include "camera.h"
 #include "timer.h"
 #include "scripting.h"
 #include "preferences.h"
-#include "imagehandler.h"
-
 #include "gui.h"
 #include "platform.h"
+#include "version.h"
 
 struct PNFrameQueue
 {
@@ -185,28 +185,166 @@ void clear_queued_data()
     pthread_mutex_unlock(&frame_queue_mutex);
 }
 
+// Write frame data to a fits file
+bool save_frame(PNFrame *frame, TimerTimestamp timestamp, float camera_temperature, char *filepath)
+{
+    fitsfile *fptr;
+    int status = 0;
+    char fitserr[128];
+
+    // Create a new fits file
+    if (fits_create_file(&fptr, filepath, &status))
+    {
+        pn_log("Unable to save file. fitsio error %d", status);
+        while (fits_read_errmsg(fitserr))
+            pn_log(fitserr);
+
+        return false;
+    }
+
+    // Create the primary array image (16-bit short integer pixels
+    long size[2] = { frame->width, frame->height };
+    fits_create_img(fptr, USHORT_IMG, 2, size, &status);
+
+    // Write header keys
+    switch (pn_preference_char(OBJECT_TYPE))
+    {
+        case OBJECT_DARK:
+            fits_update_key(fptr, TSTRING, "OBJECT", "Dark", "Object name", &status);
+            break;
+        case OBJECT_FLAT:
+            fits_update_key(fptr, TSTRING, "OBJECT", "Flat Field", "Object name", &status);
+            break;
+        default:
+        {
+            char *object_name = pn_preference_string(OBJECT_NAME);
+            fits_update_key(fptr, TSTRING, "OBJECT", (void *)object_name, "Object name", &status);
+            free(object_name);
+            break;
+        }
+    }
+
+    long exposure_time = pn_preference_char(EXPOSURE_TIME);
+    fits_update_key(fptr, TLONG, "EXPTIME", &exposure_time, "Actual integration time (sec)", &status);
+
+    char *observers = pn_preference_string(OBSERVERS);
+    fits_update_key(fptr, TSTRING, "OBSERVER", (void *)observers, "Observers", &status);
+    free(observers);
+
+    char *observatory = pn_preference_string(OBSERVATORY);
+    fits_update_key(fptr, TSTRING, "OBSERVAT", (void *)observatory, "Observatory", &status);
+    free(observatory);
+
+    char *telescope = pn_preference_string(TELESCOPE);
+    fits_update_key(fptr, TSTRING, "TELESCOP", (void *)telescope, "Telescope name", &status);
+    free(telescope);
+
+    char *instrument = pn_preference_string(INSTRUMENT);
+    fits_update_key(fptr, TSTRING, "INSTRUME", (void *)instrument, "Instrument name", &status);
+    free(instrument);
+
+    fits_update_key(fptr, TSTRING, "PROG-VER", (void *)program_version() , "Acquisition program version reported by git", &status);
+
+    if (pn_preference_char(CAMERA_OVERSCAN_ENABLED))
+    {
+        char buf[25];
+        unsigned char skip = pn_preference_char(CAMERA_OVERSCAN_SKIP_COLS);
+        unsigned char bias = pn_preference_char(CAMERA_OVERSCAN_BIAS_COLS);
+        unsigned char superpixel = pn_preference_char(CAMERA_PIXEL_SIZE);
+        sprintf(buf, "[%d, %d, %d, %d]", 0, frame->width - (skip + bias)/superpixel, 0, frame->height);
+        fits_update_key(fptr, TSTRING, "IMAG-RGN", buf, "Frame image subregion", &status);
+
+        sprintf(buf, "[%d, %d, %d, %d]", frame->width - skip/superpixel, frame->width, 0, frame->height);
+        fits_update_key(fptr, TSTRING, "BIAS-RGN", buf, "Frame bias subregion", &status);
+    }
+
+    // Trigger timestamp defines the *start* of the frame
+    TimerTimestamp start = timestamp;
+    TimerTimestamp end = start; end.seconds += exposure_time;
+    end = pn_timestamp_normalize(end);
+
+    if (timestamp.valid)
+    {
+        char datebuf[15], gpstimebuf[15];
+        sprintf(datebuf, "%04d-%02d-%02d", start.year, start.month, start.day);
+        sprintf(gpstimebuf, "%02d:%02d:%02d", start.hours, start.minutes, start.seconds);
+
+        // Used by ImageJ and other programs
+        fits_update_key(fptr, TSTRING, "UT_DATE", datebuf, "Exposure start date (GPS)", &status);
+        fits_update_key(fptr, TSTRING, "UT_TIME", gpstimebuf, "Exposure start time (GPS)", &status);
+
+        // Used by tsreduce
+        fits_update_key(fptr, TSTRING, "UTC-DATE", datebuf, "Exposure start date (GPS)", &status);
+        fits_update_key(fptr, TSTRING, "UTC-BEG", gpstimebuf, "Exposure start time (GPS)", &status);
+
+        sprintf(gpstimebuf, "%02d:%02d:%02d", end.hours, end.minutes, end.seconds);
+        fits_update_key(fptr, TSTRING, "UTC-END", gpstimebuf, "Exposure end time (GPS)", &status);
+        fits_update_key(fptr, TLOGICAL, "GPS-LOCK", &start.locked, "GPS time locked", &status);
+    }
+    else
+        fits_update_key(fptr, TLOGICAL, "GPS-VALID", &timestamp.valid, "GPS timestamp unavailable", &status);
+
+    time_t pctime = time(NULL);
+
+    char timebuf[15];
+    strftime(timebuf, 15, "%Y-%m-%d", gmtime(&pctime));
+    fits_update_key(fptr, TSTRING, "PC-DATE", (void *)timebuf, "PC Date when frame was saved to disk", &status);
+
+    strftime(timebuf, 15, "%H:%M:%S", gmtime(&pctime));
+    fits_update_key(fptr, TSTRING, "PC-TIME", (void *)timebuf, "PC Time when frame was saved to disk", &status);
+
+    // Camera temperature
+    char tempbuf[15];
+    sprintf(tempbuf, "%0.02f", camera_temperature);
+    fits_update_key(fptr, TSTRING, "CCD-TEMP", (void *)tempbuf, "CCD temperature at end of exposure in deg C", &status);
+    fits_update_key(fptr, TBYTE,   "CCD-PORT", (uint8_t[]){pn_preference_char(CAMERA_READPORT_MODE)},  "CCD Readout port index", &status);
+    fits_update_key(fptr, TBYTE,   "CCD-RATE", (uint8_t[]){pn_preference_char(CAMERA_READSPEED_MODE)}, "CCD Readout rate index", &status);
+    fits_update_key(fptr, TBYTE,   "CCD-GAIN", (uint8_t[]){pn_preference_char(CAMERA_GAIN_MODE)},      "CCD Readout gain index", &status);
+
+    // Write the frame data to the image and close the file
+    fits_write_img(fptr, TUSHORT, 1, frame->width*frame->height, frame->data, &status);
+    fits_close_file(fptr, &status);
+
+    // Log any error messages
+    while (fits_read_errmsg(fitserr))
+        pn_log("cfitsio error: %s", fitserr);
+
+    return true;
+}
+
 void process_framedata(PNFrame *frame)
 {
     TimerTimestamp timestamp = pop_trigger_timestamp();
     pn_log("Frame downloaded");
-
-    // Display the frame in ds9
-    pn_save_preview(frame, timestamp);
-    scripting_update_preview(scripting);
+    float camera_temperature = pn_camera_temperature();
 
     if (pn_preference_char(SAVE_FRAMES))
     {
-        char *filename = pn_save_frame(frame, timestamp, camera);
-        if (filename == NULL)
+        // Construct the output filepath from the output dir, run prefix, and run number.
+        // Saving will fail if a file with the same name already exists
+        char *filepath;
+        int run_number = pn_preference_int(RUN_NUMBER);
+        char *output_dir = pn_preference_string(OUTPUT_DIR);
+        char *run_prefix = pn_preference_string(RUN_PREFIX);
+        asprintf(&filepath, "%s/%s-%04d.fits.gz", output_dir, run_prefix, run_number);
+        free(run_prefix);
+        free(output_dir);
+
+        pn_log("Saving frame %s", filepath);
+        if (!save_frame(frame, timestamp, camera_temperature, filepath))
         {
             pn_log("Save failed. Discarding frame");
             return;
         }
 
         pn_preference_increment_framecount();
-        scripting_notify_frame(scripting, filename);
-        free(filename);
+        scripting_notify_frame(scripting, filepath);
+        free(filepath);
     }
+
+    // Update frame preview
+    save_frame(frame, timestamp, camera_temperature, "!preview.fits.gz");
+    scripting_update_preview(scripting);
 }
 
 void trigger_fatal_error(char *message)
