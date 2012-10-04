54b60205ba91dae6c30af3a2da5983b00c159e2e
Paul Chote <paul@chote.net>
2012-10-04 17:05:14 +1300

    Use tsreduce preview for online previews

--- preview.sh
+++ preview.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+# Load configuration overrides if defined
+if [ -f config.sh ]; then source ./config.sh; fi
+
+# Default platescale for puoko-nui camera at 2x2 binning = 0.66 arcsec / px
+if [ -z "${CAMERA_PLATESCALE}" ]; then CAMERA_PLATESCALE=0.66; fi
+
 # Open a new copy of ds9 if it is not available, otherwise preview
 if [ "$(xpaaccess -n Online_Preview)" = 0 ]; then
-	echo "Restarting ds9. Frame will not be previewed"
-    ds9 -title Online_Preview &> /dev/null &
-else
-    xpaset -p Online_Preview file $(pwd)/preview.fits.gz
+	./startup.sh
 fi
+
+tsreduce preview $(pwd)/preview.fits.gz Online_Preview ${CAMERA_PLATESCALE}
--- startup.sh
+++ startup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-DS9_STARTUP_TIMEOUT=10
-DS9_WINDOW_WIDTH=550
-DS9_WINDOW_HEIGHT=512
+
+# Load configuration overrides if defined
+if [ -f config.sh ]; then source ./config.sh; fi
+
+if [ -z "${DS9_STARTUP_TIMEOUT}" ]; then DS9_STARTUP_TIMEOUT=10; fi
+if [ -z "${DS9_WINDOW_WIDTH}" ]; then DS9_WINDOW_WIDTH=550; fi
+if [ -z "${DS9_WINDOW_HEIGHT}" ]; then DS9_WINDOW_HEIGHT=600; fi
 
 # Called at program startup to initialize ds9
 if [[ "$(xpaaccess Online_Preview)" == 'no' ]]; then
@@ -10,21 +14,21 @@ if [[ "$(xpaaccess Online_Preview)" == 'no' ]]; then
 fi
 
 # Wait for ds9 to open then resize the window
-for i in  $(seq 1 1 ${DS9_STARTUP_TIMEOUT}); do
+for i in $(seq 1 1 ${DS9_STARTUP_TIMEOUT}); do
 	if [[ "$(xpaaccess Online_Preview)" == 'yes' ]]; then
+		xpaset -p Online_Preview view layout vertical
 		xpaset -p Online_Preview view filename no
+		xpaset -p Online_Preview view object no
 		xpaset -p Online_Preview view wcs no
 		xpaset -p Online_Preview view physical no
-		xpaset -p Online_Preview view image no
 		xpaset -p Online_Preview view frame no
 		xpaset -p Online_Preview view buttons no
 		xpaset -p Online_Preview view colorbar no
-		xpaset -p Online_Preview view magnifier no
-		xpaset -p Online_Preview view panner no
 		xpaset -p Online_Preview scale mode zscale
 		xpaset -p Online_Preview orient x
 		xpaset -p Online_Preview width ${DS9_WINDOW_WIDTH}
 		xpaset -p Online_Preview height ${DS9_WINDOW_HEIGHT}
+		xpaset -p Online_Preview background black
 		exit 0
 	fi
 	sleep 1
