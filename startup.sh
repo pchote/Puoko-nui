45e8210219af9926f7d9119048cef4b382ad7710
Paul Chote <paul@chote.net>
2012-10-04 14:14:12 +1300

    Remove unnecessary UI elements from ds9 preview

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DS9_STARTUP_TIMEOUT=10
-DS9_WINDOW_WIDTH=600
+DS9_WINDOW_WIDTH=550
 DS9_WINDOW_HEIGHT=512
 
 # Called at program startup to initialize ds9
@@ -12,8 +12,19 @@ fi
 # Wait for ds9 to open then resize the window
 for i in  $(seq 1 1 ${DS9_STARTUP_TIMEOUT}); do
 	if [[ "$(xpaaccess Online_Preview)" == 'yes' ]]; then
+		xpaset -p Online_Preview view filename no
+		xpaset -p Online_Preview view wcs no
+		xpaset -p Online_Preview view physical no
+		xpaset -p Online_Preview view image no
+		xpaset -p Online_Preview view frame no
+		xpaset -p Online_Preview view buttons no
+		xpaset -p Online_Preview view colorbar no
+		xpaset -p Online_Preview view magnifier no
+		xpaset -p Online_Preview view panner no
+		xpaset -p Online_Preview scale mode zscale
+		xpaset -p Online_Preview orient x
 		xpaset -p Online_Preview width ${DS9_WINDOW_WIDTH}
-		xpaset -p Online_Preview height ${DS9_WINDOW_WIDTH}
+		xpaset -p Online_Preview height ${DS9_WINDOW_HEIGHT}
 		exit 0
 	fi
 	sleep 1
