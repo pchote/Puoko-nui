07c371d9d8909130b66827152434f4de184b65f9
Paul Chote <paul@chote.net>
2012-10-05 16:14:31 +1300

    Tweak reduction script: - Include tsreduce reduction output in acquisition log - Allow plot options to be set in config.sh

--- reduction.sh
+++ reduction.sh
@@ -4,6 +4,7 @@
 # Load configuration overrides if defined
 if [ -f config.sh ]; then source ./config.sh; fi
 if [ -z "${REDUCTION_FILENAME}" ]; then REDUCTION_FILENAME="undefined.dat"; fi
+if [ -z "${REDUCTION_PLOTOPTIONS}" ]; then REDUCTION_PLOTOPTIONS=""; fi
 
 # Path to online reduction file to update
 # By default, look for the given name in the frame directory
@@ -12,21 +13,14 @@ FILE=${DIR}/${REDUCTION_FILENAME}
 
 # Do the reduction if the file is found
 if [ -f ${FILE} ]; then
-	tsreduce update ${FILE} &> last_update.log
+	tsreduce update ${FILE}
 	if [ "$?" != '0' ]; then
-		echo 'tsreduce update FAILED. See last_update.log'
+		echo 'tsreduce update FAILED.'
 		exit 1
 	fi
 
-	# Windows does not offer a persistent plot device,
-	# so plot images which are then displayed in a web page
-	PLOTDEVICE=''
-	if [[ "${OSTYPE}" == 'msys' ]]; then
-		PLOTDEVICE='online_ts.gif/gif online_dft.gif/gif'
-	fi
-
-	tsreduce plot ${FILE} ${PLOTDEVICE} &> last_plot.log
+	tsreduce plot ${FILE} ${REDUCTION_PLOTOPTIONS}
 	if [ "$?" != '0' ]; then
-		echo 'tsreduce plot FAILED. See last_plot.log'
+		echo 'tsreduce plot FAILED.'
 	fi
 fi
