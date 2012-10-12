9a80005f4c1c95414bf4cb67a375abf1d9829404
Paul Chote <paul@chote.net>
2012-10-12 15:13:19 +1300

    update preview.sh for tsreduce changes

--- preview.sh
+++ preview.sh
@@ -3,13 +3,9 @@
 # Load configuration overrides if defined
 if [ -f config.sh ]; then source ./config.sh; fi
 
-# platescale for puoko-nui on MJUO 1m = 0.33 arcsec / px
-# tsreduce automatically corrects for binning
-if [ -z "${CAMERA_PLATESCALE}" ]; then CAMERA_PLATESCALE=0.33; fi
-
 # Open a new copy of ds9 if it is not available, otherwise preview
 if [ "$(xpaaccess -n Online_Preview)" = 0 ]; then
 	./startup.sh
 fi
 
-tsreduce preview $(pwd)/preview.fits.gz Online_Preview ${CAMERA_PLATESCALE}
+tsreduce preview $(pwd)/preview.fits.gz Online_Preview
